bab7aafabc89a22f0e6dba7c4d18a066ecc29c84
sfurukaw <shujifurukawa1213@gmail.com>
2022-07-26 11:16:52 +0900

    1st

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: sfurukaw <sfurukaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 14:31:41 by sfurukaw          #+#    #+#             */
-/*   Updated: 2022/07/26 10:52:26 by sfurukaw         ###   ########.fr       */
+/*   Updated: 2022/07/26 11:16:19 by sfurukaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*ft_left(char *left)
+char	*ft_left(char *left)
 {
 	int		i;
 	int		j;
@@ -41,7 +41,7 @@ static char	*ft_left(char *left)
 	return (new_left);
 }
 
-static char	*ft_save(char *left)
+char	*ft_save(char *left)
 {
 	int		i;
 	char	*save;
@@ -69,7 +69,7 @@ static char	*ft_save(char *left)
 	return (save);
 }
 
-static char	*ft_read(int fd, char *left)
+char	*ft_read(int fd, char *left)
 {
 	char	*tmp;
 	int		rd_size;
