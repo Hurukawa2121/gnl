0afd6b192912ebcc6ea8fd52671fc91dc22c5804
sfurukaw <shujifurukawa1213@gmail.com>
2022-07-26 09:52:20 +0900

    1st

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: sfurukaw <sfurukaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 14:32:58 by sfurukaw          #+#    #+#             */
-/*   Updated: 2022/07/26 09:47:55 by sfurukaw         ###   ########.fr       */
+/*   Updated: 2022/07/26 09:51:58 by sfurukaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,48 +64,24 @@ char	*ft_strchr(const char *s, int c)
 		return ((char *)(s + i));
 	return (NULL);
 }
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	int i;
-	int j;
-	char *ans;
-
-	if (!s1 && !s2)
-		return (ft_strdup(""));
-	if (!s1)
-		return (ft_strdup(s2));
-	if (!s2)
-		return (ft_strdup(s1));
-	ans = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!ans)
-		return (NULL);
-	i = -1;
-	while (s1[++i])
-		ans[i] = s1[i];
-	j = -1;
-	while (s2[++j])
-		ans[i + j] = s2[j];
-	ans[i + j] = '\0';
-	return (ans);
-}
 */
 
 char	*ft_strchr(char *s, int c)
 {
 	int	i;
 
-	i = 0;
 	if (!s)
-		return (0);
-	if (c == '\0')
-		return ((char *)&s[ft_strlen(s)]);
-	while (s[i] != '\0')
+		return (NULL);
+	i = 0;
+	while (s[i])
 	{
-		if (s[i] == (char) c)
-			return ((char *)&s[i]);
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
 		i++;
 	}
-	return (0);
+	if (s[i] == (char)c)
+		return ((char *)(s + i));
+	return (NULL);
 }
 
 char	*ft_strjoin(char *s1, char *s2)
@@ -120,8 +96,6 @@ char	*ft_strjoin(char *s1, char *s2)
 		return (ft_strdup(s2));
 	if (!s2)
 		return (ft_strdup(s1));
-	/*if (!s1 || !s2)
-		return (NULL);*/
 	ans = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!ans)
 		return (NULL);
